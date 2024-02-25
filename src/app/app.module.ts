5d4033fac5339b51768f7ce6b5cf04daac3b152e
unknown <joaofrassi.dev@gmail.com>
2024-02-25 09:54:50 -0300

    Chamada HTTP Get utilizando RXJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
@@ -15,6 +18,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  private readonly API = '/assets/cursos.json';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(cursos => console.log(cursos))
+    );
+  }
 }
+
+
+
