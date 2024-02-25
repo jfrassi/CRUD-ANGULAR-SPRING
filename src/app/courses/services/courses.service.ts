c3298a16b3c9e401926253528e205f9bad830b72
unknown <joaofrassi.dev@gmail.com>
2024-02-25 11:32:41 -0300

    adição de barra spinner ao carregar tabela

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { delay, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,7 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      delay(5000),
       tap(cursos => console.log(cursos))
     );
   }
